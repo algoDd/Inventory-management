49560063d170d4c6d311a23e1018395033a01154
dd <divyadeeprules@gmail.com>
2017-10-27 13:55:54 +0530

    wip

---
+++ public/javascripts/RawMaterial.js
@@ -0,0 +1,20 @@
+var app=angular.module('RMApp',['ui.router']);
+
+app.config(function($stateProvider,$urlRouterProvider)
+		{
+	$urlRouterProvider.otherwise('/');
+	$urlRouterProvider.when('/assests/html/upload.html','/');
+	$stateProvider
+	.state('home',{
+		name: 'home',
+		url:'/index',
+		templateUrl:'/assets/html/home.html'
+	}).state('RawMaterial',{
+			name: 'RawMaterial',
+			url:'/RawMaterial',
+			templateUrl:'/assets/html/RawMaterial.html'
+		});
+});
+app.controller('RMCrtl',function($scope,$http){
+	$scope.d="gvguy";
+});
